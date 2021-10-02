2db7d34a0d04e016eb8dccea85e17f5d61fb83ed
Jitesh <thejitesh@gmail.com>
2021-10-02 20:51:31 +0530

    Added sort by distance feature

--- app/src/main/java/com/skycore/restaurantsfinder/data/api/BackendApi.kt
+++ app/src/main/java/com/skycore/restaurantsfinder/data/api/BackendApi.kt
@@ -1,6 +1,7 @@
 package com.skycore.restaurantsfinder.data.api
 
 import com.skycore.restaurantsfinder.data.model.RestaurantsData
+import com.skycore.restaurantsfinder.helper.Constants.DEFAULT_SORT_ORDER_FOR_RESTAURANT_LIST
 import com.skycore.restaurantsfinder.helper.Constants.RESTAURANTS_PER_PAGE
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -17,6 +18,7 @@ interface BackendApi {
         @Query("longitude") longitude: Double,
         @Query("offset") offset: Int,
         @Query("radius") radius: Int = 0,
-        @Query("limit") limit: Int = RESTAURANTS_PER_PAGE
+        @Query("limit") limit: Int = RESTAURANTS_PER_PAGE,
+        @Query("sort_by") sortBy: String = DEFAULT_SORT_ORDER_FOR_RESTAURANT_LIST
     ): RestaurantsData
 }
--- app/src/main/java/com/skycore/restaurantsfinder/helper/Constants.kt
+++ app/src/main/java/com/skycore/restaurantsfinder/helper/Constants.kt
@@ -3,4 +3,5 @@ package com.skycore.restaurantsfinder.helper
 object Constants {
 
     const val RESTAURANTS_PER_PAGE = 15
+    const val DEFAULT_SORT_ORDER_FOR_RESTAURANT_LIST = "distance"
 }
